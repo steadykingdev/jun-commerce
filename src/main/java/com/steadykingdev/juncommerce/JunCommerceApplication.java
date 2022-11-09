fd47744914f19f219625a298893d80ef8df98904
steadykingdev <steadykingdev@gmail.com>
2022-11-09 15:50:52 +0900

    create project

---
+++ src/main/java/com/steadykingdev/juncommerce/JunCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.steadykingdev.juncommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunCommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/steadykingdev/juncommerce/JunCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.steadykingdev.juncommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
