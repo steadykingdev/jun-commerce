306ff892e9ddbec9083c1184b4a60534f39c3141
steadykingdev <steadykingdev@gmail.com>
2022-11-14 22:11:13 +0900

    querydsl 테스트

---
+++ src/test/java/com/steadykingdev/juncommerce/repository/QuerydslTest.java
@@ -0,0 +1,41 @@
+package com.steadykingdev.juncommerce.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.steadykingdev.juncommerce.entity.Item;
+import com.steadykingdev.juncommerce.entity.QItem;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+@SpringBootTest
+@Transactional
+public class QuerydslTest {
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    public void contextLoads() throws Exception {
+        //given
+        Item item = new Item("a");
+        em.persist(item);
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QItem qItem = QItem.item;
+
+        //when
+        Item result = query
+                .selectFrom(qItem)
+                .where(qItem.itemName.eq("a"))
+                .fetchOne();
+
+        //then
+        Assertions.assertThat(result).isEqualTo(item);
+        Assertions.assertThat(result.getItemName()).isEqualTo(item.getItemName());
+    }
+
+}
