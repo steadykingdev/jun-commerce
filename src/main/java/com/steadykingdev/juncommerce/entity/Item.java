33a06bba1a7a4e73901561866f9a628b43f03579
steadykingdev <steadykingdev@gmail.com>
2022-11-26 17:34:38 +0900

    item entity 생성자 함수 수정

--- src/main/java/com/steadykingdev/juncommerce/entity/Item.java
+++ src/main/java/com/steadykingdev/juncommerce/entity/Item.java
@@ -1,6 +1,7 @@
 package com.steadykingdev.juncommerce.entity;
 
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -15,25 +16,33 @@ public class Item {
     @Column(name = "item_id")
     private Long id;
     private String brand;
-    private String itemName;
-    private String itemSize;
+    private String itemname;
     private int itemPrice;
-    private int stockQuantity;
 
-    public Item(String itemName) {
-        this.itemName = itemName;
-    } // 임시로 만든 생성자 함수
-
-    public void addStock(int quantity) {
-        this.stockQuantity += quantity;
+    public Item(String brand, String itemname, int itemPrice) {
+        this.brand = brand;
+        this.itemname = itemname;
+        this.itemPrice = itemPrice;
     }
 
-    public void removeStock(int quantity) {
-        int restStock = this.stockQuantity - quantity;
-        if(restStock < 0) {
-            // throw
-        }
-        this.stockQuantity = restStock;
+    //    public void addStock(int quantity) {
+//        this.stockQuantity += quantity;
+//    }
+
+//    public void removeStock(int quantity) {
+//        int restStock = this.stockQuantity - quantity;
+//        if(restStock < 0) {
+//            // throw
+//        }
+//        this.stockQuantity = restStock;
+//    }
+
+    @Builder
+    public Item(Long id, String brand, String itemname, int itemPrice) {
+        this.id = id;
+        this.brand = brand;
+        this.itemname = itemname;
+        this.itemPrice = itemPrice;
     }
 
 }
