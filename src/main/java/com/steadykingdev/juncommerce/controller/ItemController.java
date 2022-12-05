e6039b6d2f66a59bd26c359b5759aad6c2b84fdf
steadykingdev <steadykingdev@gmail.com>
2022-12-05 18:42:11 +0900

    공통 응답 포멧 개발 및 적용

--- src/main/java/com/steadykingdev/juncommerce/controller/ItemController.java
+++ src/main/java/com/steadykingdev/juncommerce/controller/ItemController.java
@@ -1,8 +1,12 @@
 package com.steadykingdev.juncommerce.controller;
 
+import com.steadykingdev.juncommerce.common.CommonResult;
+import com.steadykingdev.juncommerce.common.ListResult;
+import com.steadykingdev.juncommerce.common.SingleResult;
 import com.steadykingdev.juncommerce.dto.SaveItemRequestDto;
 import com.steadykingdev.juncommerce.dto.ItemResponseDto;
 import com.steadykingdev.juncommerce.service.ItemService;
+import com.steadykingdev.juncommerce.service.ResponseService;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,23 +20,24 @@ import java.util.List;
 public class ItemController {
 
     private final ItemService itemService;
+    private final ResponseService responseService;
 
     @ApiOperation(value = "상품 조회", notes = "모든 상품을 조회한다")
     @GetMapping("/api/items")
-    public List<ItemResponseDto> getItems() {
-        return itemService.findItems();
+    public ListResult<ItemResponseDto> getItems() {
+        return responseService.getListResult(itemService.findItems());
     }
 
     @ApiOperation(value = "상품 조회", notes = "하나의 상품을 조회한다")
     @GetMapping("/api/item/{id}")
-    public ItemResponseDto getItem(@PathVariable("id") Long id) {
-        return itemService.findOne(id);
+    public SingleResult<ItemResponseDto> getItem(@PathVariable("id") Long id) {
+        return responseService.getSingleResult(itemService.findOne(id));
     }
 
     @ApiOperation(value = "상품 등록", notes = "상품을 등록한다")
     @PostMapping("/api/item/add")
-    public Long addItem(@RequestBody SaveItemRequestDto saveItemRequestDto) {
-        return itemService.saveItem(saveItemRequestDto);
+    public CommonResult addItem(@RequestBody SaveItemRequestDto saveItemRequestDto) {
+        return responseService.getSingleResult(itemService.saveItem(saveItemRequestDto));
     }
 
 }
