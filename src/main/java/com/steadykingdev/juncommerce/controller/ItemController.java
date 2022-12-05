d1bafd6e8d7b479c36ef7efcbec1763208637f31
steadykingdev <steadykingdev@gmail.com>
2022-12-05 18:02:30 +0900

    swagger 적용

--- src/main/java/com/steadykingdev/juncommerce/controller/ItemController.java
+++ src/main/java/com/steadykingdev/juncommerce/controller/ItemController.java
@@ -1,17 +1,13 @@
 package com.steadykingdev.juncommerce.controller;
 
-import com.steadykingdev.juncommerce.dto.ApiResult;
 import com.steadykingdev.juncommerce.dto.SaveItemRequestDto;
 import com.steadykingdev.juncommerce.dto.ItemResponseDto;
 import com.steadykingdev.juncommerce.service.ItemService;
-import com.steadykingdev.juncommerce.utils.ApiUtils;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -21,22 +17,22 @@ public class ItemController {
 
     private final ItemService itemService;
 
+    @ApiOperation(value = "상품 조회", notes = "모든 상품을 조회한다")
     @GetMapping("/api/items")
-    public ApiResult getItems() {
-        List<ItemResponseDto> items = itemService.findItems();
-        return ApiUtils.success(items);
+    public List<ItemResponseDto> getItems() {
+        return itemService.findItems();
     }
 
+    @ApiOperation(value = "상품 조회", notes = "하나의 상품을 조회한다")
     @GetMapping("/api/item/{id}")
-    public ApiResult getItem(@PathVariable("id") Long id) {
-        ItemResponseDto item = itemService.findOne(id);
-        return ApiUtils.success(item);
+    public ItemResponseDto getItem(@PathVariable("id") Long id) {
+        return itemService.findOne(id);
     }
 
+    @ApiOperation(value = "상품 등록", notes = "상품을 등록한다")
     @PostMapping("/api/item/add")
-    public ApiResult addItem(@RequestBody SaveItemRequestDto saveItemRequestDto) {
-        Long itemId = itemService.saveItem(saveItemRequestDto);
-        return ApiUtils.success(itemId);
+    public Long addItem(@RequestBody SaveItemRequestDto saveItemRequestDto) {
+        return itemService.saveItem(saveItemRequestDto);
     }
 
 }
