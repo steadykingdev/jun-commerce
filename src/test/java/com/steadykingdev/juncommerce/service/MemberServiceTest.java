016fe6579e987d86559983f083dab816099a6af9
steadykingdev <steadykingdev@gmail.com>
2023-01-03 19:29:47 +0900

    회원가입 test 완료

--- src/test/java/com/steadykingdev/juncommerce/service/MemberServiceTest.java
+++ src/test/java/com/steadykingdev/juncommerce/service/MemberServiceTest.java
@@ -3,14 +3,14 @@ package com.steadykingdev.juncommerce.service;
 import com.steadykingdev.juncommerce.dto.member.SaveMemberRequestDto;
 import com.steadykingdev.juncommerce.entity.Address;
 import com.steadykingdev.juncommerce.entity.member.Member;
-import org.assertj.core.api.Assertions;
+import com.steadykingdev.juncommerce.repository.member.MemberRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.transaction.Transactional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
@@ -18,6 +18,9 @@ class MemberServiceTest {
 
     @Autowired
     MemberService memberService;
+
+    @Autowired
+    MemberRepository memberRepository;
 
     @Test
     public void addMember() throws Exception {
@@ -29,8 +32,27 @@ class MemberServiceTest {
         Long memberId = memberService.addMember(memberDto);
 
         //then
-        Assertions.assertThat(memberId).isEqualTo(4L);
+        assertThat(memberId).isEqualTo(4L);
     }
+
+    @Test
+    public void addMemberDuplicateException() throws Exception {
+        //given
+        Address address1 = new Address("서울", "시흥대로", "1-2");
+        Member member1 = new Member(null, "kim", "김", "1234", address1);
+        memberRepository.save(member1);
+
+        //when
+        Address address2 = new Address("서울22", "시흥대로11", "1-22");
+        SaveMemberRequestDto saveMemberResponseDto = new SaveMemberRequestDto("kim", "김맨", "1111", "1111", address2);
+
+        //then
+        assertThatThrownBy(() -> memberService.addMember(saveMemberResponseDto))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("이미 존재하는 회원입니다.");
+    }
+
+
 
 
 }
