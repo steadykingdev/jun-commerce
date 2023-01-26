bce49b8197b5e7b604249877786d28822b03285d
steadykingdev <steadykingdev@gmail.com>
2023-01-26 14:58:39 +0900

    security 설정

---
+++ src/main/java/com/steadykingdev/juncommerce/config/security/SecurityConfig.java
@@ -0,0 +1,64 @@
+package com.steadykingdev.juncommerce.config.security;
+
+import com.steadykingdev.juncommerce.jwt.JwtAccessDeniedHandler;
+import com.steadykingdev.juncommerce.jwt.JwtAuthenticationEntryPoint;
+import com.steadykingdev.juncommerce.jwt.JwtSecurityConfig;
+import com.steadykingdev.juncommerce.jwt.TokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity // 기본적인 웹 보안 활성화
+@EnableGlobalMethodSecurity(prePostEnabled = true) // @PreAuthorize를 메서드 단위로 추가하기 위해 적용
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final TokenProvider tokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void configure(WebSecurity web) {
+        web
+                .ignoring()
+                .antMatchers(
+                        "/favicon.ico"
+                );
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception{
+        http
+                .httpBasic().disable() // rest api 이므로 기본설정 disable, 기본설정은 비인증시 로그인 폼 화면으로 리다이렉트된다.
+                .csrf().disable() // token 방식이므로 disable
+
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션을 사용하지 않기때문
+
+                .and()
+                .authorizeRequests() // HttpServletRequest를 사용하는 요청들에 대한 접근 제한을 설정
+                //.antMatchers("").permitAll()
+                //.anyRequest().authenticated() // 나머지 요청은 인증받아야함.
+
+                .and()
+                .apply(new JwtSecurityConfig(tokenProvider)); // JwtFilter를 addFilterBefore로 등록한 config 클래스도 적용.
+    }
+
+}
---
+++ src/main/java/com/steadykingdev/juncommerce/jwt/TokenProvider.java
@@ -0,0 +1,94 @@
+package com.steadykingdev.juncommerce.jwt;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+@Component
+@Slf4j
+public class TokenProvider implements InitializingBean {
+
+    private static final String AUTHORITIES_KEY = "auth";
+    private final String secret;
+    private final long tokenValidityInMilliseconds;
+    private Key key;
+
+    public TokenProvider(@Value("${jwt.secret}") String secret,
+                         @Value("${jwt.token-validity-in-seconds}") long tokenValidityInMilliseconds) {
+        this.secret = secret;
+        this.tokenValidityInMilliseconds = tokenValidityInMilliseconds * 1000;
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {     // Bean 생성 후 의존성 주입을 받은 후에
+        byte[] keyBytes = Decoders.BASE64.decode(secret);   // secret 값을 Base64 Decode해서
+        this.key = Keys.hmacShaKeyFor(keyBytes);            // key 변수에 할당
+    }
+
+    // 토큰 생성
+    public String createToken(Authentication authentication) {
+        String authorities = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(","));
+
+        long now = (new Date()).getTime();
+        Date validity = new Date(now + this.tokenValidityInMilliseconds);
+
+        return Jwts.builder()
+                .setSubject(authentication.getName())
+                .claim(AUTHORITIES_KEY, authorities)
+                .signWith(key, SignatureAlgorithm.HS512)
+                .setExpiration(validity)
+                .compact();
+    }
+
+    public Authentication getAuthentication(String token) { // 토큰을 파라미터로 받아서 토큰에 담겨있는 권한정보들을 이용해서 Authentication 객체를 반환
+        Claims claims = Jwts            // 토큰을 이용해서 claim을 만들어줌
+                .parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        Collection<? extends GrantedAuthority> authorities =     // 클레임에서 권한정보들을 빼냄
+                Arrays.stream(claims.get(AUTHORITIES_KEY).toString().split(","))
+                        .map(SimpleGrantedAuthority::new)
+                        .collect(Collectors.toList());
+
+        User principal = new User(claims.getSubject(), "", authorities); // 권한정보들을 이용해서 user객체를 만들어줌
+
+        return new UsernamePasswordAuthenticationToken(principal, token, authorities); // user객체와 토큰, 권한정보들을 이용해서 authentication 객체를 반환
+    }
+
+    // 토큰 유효성 검증
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token); // 토큰을 파싱
+            return true;
+        } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
+            log.info("잘못된 JWT 서명입니다.");
+        } catch (ExpiredJwtException e) {
+            log.info("만료된 JWT 토큰입니다.");
+        } catch (UnsupportedJwtException e) {
+            log.info("지원되지 않는 JWT 토큰입니다.");
+        } catch (IllegalArgumentException e) {
+            log.info("JWT 토큰이 잘못되었습니다.");
+        }
+        return false;
+    }
+}
